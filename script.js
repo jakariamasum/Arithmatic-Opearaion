5c3d8ff2d5203e7b041e26cc2520fb5de0148462
jakariamasum <jmphy698@gmail.com>
2024-02-21 20:49:21 +0600

    done

--- script.js
+++ script.js
@@ -6,18 +6,30 @@ function addition() {
     console.log(firstNumber, secondNumber);
     let result = firstNumber + secondNumber;
     document.getElementById('result').value = result;
+    changeButtonColor('additionButton');
+    restoreButtonColor('subtractButton');
+    restoreButtonColor('multiplyButton');
+    restoreButtonColor('divisionButton');
 }
 
 function subtraction() {
     getData();
     let result = firstNumber - secondNumber;
     document.getElementById('result').value = result;
+    changeButtonColor('subtractButton');
+    restoreButtonColor('additionButton');
+    restoreButtonColor('multiplyButton');
+    restoreButtonColor('divisionButton');
 }
 
 function multiplication() {
     getData();
     let result = firstNumber * secondNumber;
     document.getElementById('result').value = result;
+    changeButtonColor('multiplyButton');
+    restoreButtonColor('subtractButton');
+    restoreButtonColor('additionButton');
+    restoreButtonColor('divisionButton');
 }
 
 function division() {
@@ -28,6 +40,10 @@ function division() {
     }
     let result = firstNumber / secondNumber;
     document.getElementById('result').value = result;
+    changeButtonColor('divisionButton');
+    restoreButtonColor('subtractButton');
+    restoreButtonColor('multiplyButton');
+    restoreButtonColor('additionButton');
 }
 
 function clearForm() {
@@ -38,4 +54,14 @@ function clearForm() {
 function getData() {
     firstNumber = parseFloat(document.getElementById('firstNumber').value);
     secondNumber = parseFloat(document.getElementById('secondNumber').value);
+}
+
+function changeButtonColor(buttonId) {
+    document.getElementById(buttonId).style.backgroundColor = "#6c757d";
+    document.getElementById(buttonId).style.borderColor = "#6c757d";
+}
+function restoreButtonColor(buttonId) {
+    document.getElementById(buttonId).style.backgroundColor = "#0d6efd";
+    document.getElementById(buttonId).style.borderColor = "#0d6efd";
+
 }
